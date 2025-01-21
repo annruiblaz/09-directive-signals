beb42dc1222277e3123197af0f40e7533e9d67e2
annruiblaz <anruiblaz@gmail.com>
2025-01-21 14:41:26 +0100

    signal con petición http

--- src/app/signals/interfaces/interface.ts
+++ src/app/signals/interfaces/interface.ts
@@ -2,3 +2,21 @@ export interface MenuItem {
     title: string;
     route: string;
 }
+
+export interface SingleUserResponse {
+    data:    User;
+    support: Support;
+}
+
+export interface User {
+    id:         number;
+    email:      string;
+    first_name: string;
+    last_name:  string;
+    avatar:     string;
+}
+
+export interface Support {
+    url:  string;
+    text: string;
+}
